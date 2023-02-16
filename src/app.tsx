010615c07a9b823870ba9202b2dc060368594de0
Chalo Fernandez <chalofa@gmail.com>
2023-02-16 10:52:40 -0600

    Updates to homepage

--- src/app.tsx
+++ src/app.tsx
@@ -5,19 +5,15 @@ import './app.css'
 export function App() {
   return (
     <>
-      <div>
-        <img src={heroLogo} class="logo herocoders" alt="HeroCoders logo" />
-      </div>
-
-      <h1>HeroCoders</h1>
+      <h1>
+        <img src={heroLogo} class="logo herocoders" alt="HeroCoders" />
+      </h1>
 
       <div class="card">
-        <marquee>
-          Coming <code>soon</code>!
-        </marquee>
+        We are the <strong>heroes</strong> your code needs!
       </div>
 
-      <ul class="read-the-docs">
+      <ul class="list-unstyled">
         <li><code>Ruby on Rails</code></li>
         <li><code>React</code></li>
         <li><code>Web development</code></li>
